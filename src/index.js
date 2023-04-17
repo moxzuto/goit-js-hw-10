d778f83a62d811a26bef3d7481b354fe6fb80665
Ilya Kulish <ilyakulish06@outlook.com>
2023-04-18 02:00:08 +0400

    work done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-import './css/styles.css';
+import "./css/styles.css";
+import debounce from "lodash.debounce";
+import Notiflix, { Notify } from "notiflix";
+import { fetchCountries } from "./fetchCountries";
+
+const searchBoxEl = document.getElementById("search-box");
+const countryListEl = document.querySelector(".country-list");
+const countryInfoEl = document.querySelector(".country-info");
+
+const handleSearchCountry = async (event) => {
+  const searchQuery = event.target.value.trim();
+  countryListEl.innerHTML = "";
+
+  if (searchQuery !== "") {
+    try {
+      const countries = await fetchCountries(searchQuery);
+
+      if (2 <= countries.length && countries.length <= 10) {
+        const countriesMarkup = countries
+          .map(
+            (country) => `
+              <li class="list-item">
+                <img class="flag" src="${country.flags.png}" width="80px" />
+                ${country.name.common}
+              </li>
+            `
+          )
+          .join("");
+
+        countryListEl.insertAdjacentHTML("beforeend", countriesMarkup);
+      }
+
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          "Too many matches found. Please enter a more specific name."
+        );
+      }
+
+      if (countries.length === 1) {
+        const country = countries[0];
+        const countryMarkup = `
+          <h2>
+            <img class="flag" src="${country.flags.png}" width="80px" />
+            ${country.name.common}
+          </h2>
+          <p>Capital: ${country.capital}</p>
+          <p>Population: ${country.population}</p>
+          <p>Languages: ${Object.values(country.languages).join(", ")}</p>
+        `;
+        countryInfoEl.innerHTML = countryMarkup;
+      }
+    } catch (error) {
+      Notiflix.Notify.failure("Oops, there is no country with that name");
+    }
+  }
+};
 
 const DEBOUNCE_DELAY = 300;
+
+searchBoxEl.addEventListener(
+  "input",
+  debounce(handleSearchCountry, DEBOUNCE_DELAY)
+);
